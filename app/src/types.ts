fce2400df65d253fbaa92ea2ca3838a7ff5f1405
Brian Craig <briancraigok@gmail.com>
2021-09-29 13:49:21 -0300

    feat: add types

--- app/src/types.ts
+++ app/src/types.ts
@@ -4,3 +4,44 @@ export enum STATUS {
   BOUGHT,
   OTHER
 };
+
+/**
+ * Hexadecimal string ex: "0fax"
+ */
+export type HexaString = string;
+
+/**
+ * Hexadecimal string ex: "0fax"
+ */
+export type ZeroXHexaString = string;
+
+export interface Auction {
+  id: HexaString,
+  tx: HexaString,
+  version: {
+    block: HexaString,
+    index: HexaString
+  },
+  price: HexaString,
+  timestamp: number,
+  endTimestamp?: number,
+  status: STATUS
+};
+
+export type AuctionMap = { [key: string]: Auction }
+
+export interface TxLog {
+  address: ZeroXHexaString,
+  topics: [
+    ZeroXHexaString
+  ],
+  data:
+  ZeroXHexaString, blockNumber: ZeroXHexaString,
+  timeStamp: ZeroXHexaString,
+  gasPrice: ZeroXHexaString,
+  gasUsed: ZeroXHexaString,
+  logIndex: ZeroXHexaString,
+  transactionHash:
+  ZeroXHexaString,
+  transactionIndex: ZeroXHexaString
+}
